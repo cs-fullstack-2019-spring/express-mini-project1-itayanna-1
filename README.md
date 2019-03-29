d7ec34bd9fde153e0a3b3170267659e33e504924
Kenn <kenn+git@code-crew.org>
2019-03-29 15:05:32 -0500

    Graded 03-29-2019 15:04:59

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Make a site for super heros to apply to work in Memphis. Use the Pug views provi
 
 ### Challenge
 Make a proper edit page by asking the user which ID to edit, then providing the existing data to make changes.
+<hr>
+0.5 points. I was able to fix the home page to see the data in the database, but the model is not set up properly to display the data. Also, find, delete, and add is not working properly.
--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,18 @@ var HeroCollection = require('../models/heroModel');
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('/', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    HeroCollection.find({}, (error, results) =>
+        res.render('index', {allEntries: results}));
 });
 
 
 router.route('/add')
-    .get (function (req, res){
+    .get(function (req, res) {
         res.render('add');
     })
 
-    .post(function (req,res) {
+    .post(function (req, res) {
         HeroCollection.create(
             {
                 id: req.body.id,
@@ -25,27 +26,28 @@ router.route('/add')
                 combat: req.body.combat,
                 wealth: req.body.wealth,
                 image: req.body.image
-            }, (errors)=>{
+            }, (errors) => {
                 if (errors) res.send(errors);
-                else res.render('add')});
+                else res.render('add')
+            });
 
     });
 
 
 router.route('/delete')
 
-    .post(function(req, res){
-        HeroCollection.deleteOne({id:req.body.id},
-            (error)=>{
-                if(error) res.send(error);
+    .post(function (req, res) {
+        HeroCollection.deleteOne({id: req.body.id},
+            (error) => {
+                if (error) res.send(error);
                 else res.render('delete');
             })
 
 
-    .get(function (req,res) {
-        res.render('delete')
-    })
-});
+            .get(function (req, res) {
+                res.render('delete')
+            })
+    });
 
 // router.get('/edit',(req, res)=>{
 //     HeroCollection.updateOne({id:req.query.id},{
@@ -63,18 +65,17 @@ router.route('/delete')
 //     res.send('editChange')
 // });
 
-router.get('/error',(req, res)=>{
+router.get('/error', (req, res) => {
     res.render('error')
 });
 
-router.get('/find',(req, res)=>{
-    HeroCollection.find({id:req.query.id},
-        (error, results)=>{
-            if(error) res.render('error');
-            else res.render('find',{findResults:results})
+router.get('/find', (req, res) => {
+    HeroCollection.find({id: req.query.id},
+        (error, results) => {
+            if (error) res.render('error');
+            else res.render('find', {findResults: results})
         });
 });
-
 
 
 module.exports = router;
